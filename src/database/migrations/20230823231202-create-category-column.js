cc35d19ff94152be60fe22ad67f45f768383d840
Bruna Padilha da Costa <114956460+brunapcosta@users.noreply.github.com>
2023-09-21 18:35:32 -0300

    migrations

--- src/database/migrations/20230823231202-create-category-column.js
+++ src/database/migrations/20230823231202-create-category-column.js
@@ -11,7 +11,7 @@ module.exports = {
     })
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     await queryInterface.removeColumn('products', 'category_id')
   },
 };
