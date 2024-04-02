a58e12a2efe00bb2d5e051c0e1134b98ac4c97b9
brunapcosta <brunaa.jornalismo@gmail.com>
2024-04-02 20:31:12 -0300

    remove-corsOptions

--- src/app.js
+++ src/app.js
@@ -11,16 +11,16 @@ import cors from 'cors'
 
 import "./database"
 
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-}
+// const corsOptions = {
+//   origin: '*',
+//   credentials: true,
+// }
 
 class App {
 
   constructor() {
     this.app = express()
-    this.app.use(cors(corsOptions))
+    this.app.use(cors())
 
     //   {
     //   origin: 'http://localhost:3000',
