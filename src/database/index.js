69495011c9adf41ce093b94d9e0b623bd67d252d
brunapcosta <brunaa.jornalismo@gmail.com>
2024-04-10 18:58:59 -0300

    config-postgres2

--- src/database/index.js
+++ src/database/index.js
@@ -24,7 +24,7 @@ class Database {
 
     init() {
         this.connection = new Sequelize(
-            'postgresql://postgres:hXhwvDsmeJmhOSVBeUvzExqJtYJetTKL@viaduct.proxy.rlwy.net:34337'
+            'postgresql://postgres:hXhwvDsmeJmhOSVBeUvzExqJtYJetTKL@viaduct.proxy.rlwy.net:34337.railway'
         )
         models
             .map((model) => model.init(this.connection))
